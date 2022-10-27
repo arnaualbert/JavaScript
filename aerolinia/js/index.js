3a8f515c014436ba4f13fb11a32528c6761084d4
Arnau Albert <75489909+arnaualbert@users.noreply.github.com>
2022-10-27 21:05:37 +0200

    Add files via upload

--- aerolinia/js/index.js
+++ aerolinia/js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // crea un nuevo objeto `Date`
     var today = new Date();
 
-    document.getElementById("fecha").innerHTML=today;
+    document.getElementById("fecha").innerHTML = today;
     // // obtener la fecha de hoy en formato `MM/DD/YYYY`
     var now = today.toLocaleDateString('en-GB');
 
@@ -16,27 +16,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("bt3").style.display = "none";
 
+    document.getElementById("buscar").style.display = "none"
+
     // document.getElementById("origen").
 
     document.getElementById("bt3").addEventListener("click", function () {
-       //console.log(getCookie("galeta"))
-       // borrarcookie("cookie")
+        //console.log(getCookie("galeta"))
+        // borrarcookie("cookie")
         // eliminarCookies();
         document.cookie = "galeta=notexist;expires=Thu, 01 Jan 1970 00:00:00 UTC";
 
         document.getElementById("divlogin").style.display = "none";
 
         document.getElementById("divregistro").style.display = "none";
-    
+
         document.getElementById("divvuelos").style.display = "none";
 
         document.getElementById("response").style.display = "none";
 
-        document.getElementById("bt3").style.display="none";
+        document.getElementById("bt3").style.display = "none";
 
-        document.getElementById("bt2").style.display="block";
+        document.getElementById("bt2").style.display = "block";
 
-        document.getElementById("bt1").style.display="block";
+        document.getElementById("bt1").style.display = "block";
     })
 
     document.getElementById("bt1").addEventListener("click", function () {
@@ -74,11 +76,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("response").innerHTML = responseServer;
                 if (responseServer == "Welcome Admin") {
                     checkCookie("galeta", username);
-                    document.getElementById("bt1").style.display="none";
-                    document.getElementById("bt2").style.display="none";
+                    document.getElementById("bt1").style.display = "none";
+                    document.getElementById("bt2").style.display = "none";
                     document.getElementById("divlogin").style.display = "none";
                     document.getElementById("divvuelos").style.display = "block";
-                    document.getElementById("bt3").style.display="block";
+                    document.getElementById("bt3").style.display = "block";
                     loadoptions();
                 };
             }
@@ -114,13 +116,21 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
     document.getElementById("btvuelos").addEventListener("click", function () {
-        document.getElementById("response").style.display="block";
+        document.getElementById("response").style.display = "block";
+        // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        // var d = new Date(salida);
+        // var dayName = d.toString().split(' ')[0];
+        // console.log(dayName)
         var origen = document.getElementById("origen").value;
         var destino = document.getElementById("destino").value;
         var salida = document.getElementById("salida").value;
+        var d = new Date(salida);
+        var dayName = d.toString().split(' ')[0];
+        console.log(dayName)
         var vuelta = document.getElementById("vuelta").value;
         var pasajeros = document.getElementById("pasajeros").value;
         let viaje = {
+            dayName: dayName,
             origen: origen,
             destino: destino,
             salida: salida,
@@ -128,9 +138,9 @@ document.addEventListener("DOMContentLoaded", function () {
             pasajeros: pasajeros
         };
         console.log(viaje);
-        // if (origen == "" || destino == "" || salida == "" || vuelta == "" || pasajeros == ""){
-        //     alert("rellena todos los campos")
-        // }else if (origen == destino){alert("el orige y el destino son el mismo")}else{alert("buscando viajes")}
+        if (origen == "" || destino == "" || salida == "" || vuelta == "" || pasajeros == "") {
+            alert("rellena todos los campos")
+        } else if (origen == destino) { alert("el orige y el destino son el mismo") } else { alert("buscando viajes") }
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "./php/buscar.php");//obrir connexio
         xhr.send(JSON.stringify(viaje));//enviament de dades: objeto a JSON antes del envio
@@ -142,12 +152,52 @@ document.addEventListener("DOMContentLoaded", function () {
                 //alert(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
                 //xhr.response es un JSON que viene desde PHP
                 let responseServer = JSON.parse(xhr.response);//reconvertirlo/parsearlo a variable JS
-                document.getElementById("response").innerHTML = responseServer;
+                document.getElementById("info").innerHTML = responseServer;
                 // if (responseServer == "Welcome Admin") { checkCookie("cookie", username); };
             }
         }
-    })
+        //nuevo
+        // xhr.abort();
+        let ls = new XMLHttpRequest();
+        ls.open("POST", "./php/salidas.php");
+        ls.send(JSON.stringify(viaje));
+        ls.onload = function () {
 
+            if (ls.status != 200) {
+                alert(`Error ${ls.status}: ${ls.statusText}`);
+            } else {
+                let responseServer = JSON.parse(ls.response);
+                document.getElementById("tt").innerHTML = responseServer; // cambiar tt por salidas
+                // nuevo
+                var data = JSON.parse(ls.response);
+
+                for (var i = 0; i < data.length; i++) {
+                    var option = document.createElement("option");
+                    option.value = data[i];
+                    option.innerHTML = data[i];
+                    document.getElementById("salidas").appendChild(option)
+                };
+
+
+                ///
+            }
+        }
+        // ls.abort();
+        // lo de arriba hasta //nuevo es nuevo
+        if (origen == "" || destino == "" || salida == "" || vuelta == "" || pasajeros == "") {
+            alert("rellena todos los campos")
+        } else if (origen == destino) { alert("el orige y el destino son el mismo") } else {
+            alert("buscando viajes")
+            if ((dayName == "Mon") || (dayName == "Fri") || (dayName == "Sat")) {
+                document.getElementById("divvuelos").style.display = "none";
+                document.getElementById("tt").style.display = "none";
+                document.getElementById("buscar").style.display = "block";
+                // salidas(dayName);
+            } else {
+                alert("Solo hay vuelos los Lunes, los Viernes y los Sabados")
+            }
+        }
+    })
 });
 
 
@@ -183,7 +233,7 @@ function validaNomCognoms(value) {
 }
 
 function setCookie(cname, cvalue) {
-    document.cookie = cname + "=" + cvalue ;
+    document.cookie = cname + "=" + cvalue;
 }
 
 function getCookie(cname) {
@@ -242,21 +292,21 @@ const borrarcookie = (cookie) => {
 //     }
 // }
 
-function loadoptions(){
+function loadoptions() {
     let request = new XMLHttpRequest()
-    request.open("GET","./php/vuelos.php");
+    request.open("GET", "./php/vuelos.php");
     request.send();
-    request.onload = function (){
-        var data= JSON.parse(request.response);
+    request.onload = function () {
+        var data = JSON.parse(request.response);
 
-        for(var i=0;i < data.length;i++){
+        for (var i = 0; i < data.length; i++) {
             var option = document.createElement("option");
             option.value = data[i];
             option.innerHTML = data[i];
             document.getElementById("origen").appendChild(option)
         };
 
-        for(var i=0;i < data.length;i++){
+        for (var i = 0; i < data.length; i++) {
             var option = document.createElement("option");
             option.value = data[i];
             option.innerHTML = data[i];
@@ -264,3 +314,19 @@ function loadoptions(){
         };
     }
 }
+
+// function salidas(dayname) {
+//     let request = new XMLHttpRequest()
+//     request.open("POST", "./php/salidas.php");
+//     request.send(JSON.stringify(dayname));
+//     request.onload = function () {
+//         var data = JSON.parse(request.response);
+
+//         for (var i = 0; i < data.length; i++) {
+//             var option = document.createElement("option");
+//             option.value = data[i];
+//             option.innerHTML = data[i];
+//             document.getElementById("salidas").appendChild(option)
+//         };
+//     }
+// }
