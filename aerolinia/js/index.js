7cd1da255416d0a239ece0d942bc3d3b973e0d83
Arnau Albert <75489909+arnaualbert@users.noreply.github.com>
2022-10-28 13:38:25 +0200

    Add files via upload

--- aerolinia/js/index.js
+++ aerolinia/js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     // crea un nuevo objeto `Date`
     var today = new Date();
 
@@ -18,9 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("buscar").style.display = "none"
 
+    document.getElementById("usuario").style.display = "none";
+
+    document.getElementById("datos").style.display = "none";
+
     // document.getElementById("origen").
 
-    document.getElementById("bt3").addEventListener("click", function () {
+    document.getElementById("bt3").addEventListener("click", function() {
         //console.log(getCookie("galeta"))
         // borrarcookie("cookie")
         // eliminarCookies();
@@ -36,43 +40,49 @@ document.addEventListener("DOMContentLoaded", function () {
 
         document.getElementById("bt3").style.display = "none";
 
+        document.getElementById("datos").style.display = "none";
+
+        document.getElementById("usuario").style.display = "none";
+
         document.getElementById("bt2").style.display = "block";
 
         document.getElementById("bt1").style.display = "block";
     })
 
-    document.getElementById("bt1").addEventListener("click", function () {
+    document.getElementById("bt1").addEventListener("click", function() {
         document.getElementById("divlogin").style.display = "block";
         document.getElementById("divregistro").style.display = "none";
         document.getElementById("divvuelos").style.display = "none";
     })
 
-    document.getElementById("bt2").addEventListener("click", function () {
+    document.getElementById("bt2").addEventListener("click", function() {
         document.getElementById("divlogin").style.display = "none";
         document.getElementById("divvuelos").style.display = "none";
         document.getElementById("divregistro").style.display = "block";
     })
 
-    document.getElementById("btLogin").addEventListener("click", function () {
+    document.getElementById("btLogin").addEventListener("click", function() {
         var username = document.getElementById("username").value;
+        document.getElementById("nombrerelleno").value = username;
+        document.getElementById("nomini").innerHTML = username;
         var pwd = document.getElementById("pwd").value;
         let user2 = {
             username: username,
             pwd: pwd
         };
         console.log(user2)
-        //enviar aquest objecte al servidor:
+            //enviar aquest objecte al servidor:
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", "./php/server.php");//obrir connexio
-        xhr.send(JSON.stringify(user2));//enviament de dades: objeto a JSON antes del envio
-        xhr.onload = function () {//esperar a rebre dades
+        xhr.open("POST", "./php/server.php"); //obrir connexio
+        xhr.send(JSON.stringify(user2)); //enviament de dades: objeto a JSON antes del envio
+        xhr.onload = function() { //esperar a rebre dades
 
             if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
                 alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
             } else { // muestra el resultado
                 //alert(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
                 //xhr.response es un JSON que viene desde PHP
-                let responseServer = JSON.parse(xhr.response);//reconvertirlo/parsearlo a variable JS
+                let responseServer = JSON.parse(xhr.response); //reconvertirlo/parsearlo a variable JS
                 document.getElementById("response").innerHTML = responseServer;
                 if (responseServer == "Welcome Admin") {
                     checkCookie("galeta", username);
@@ -87,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 
-    document.getElementById("btregistro").addEventListener("click", function () {
+    document.getElementById("btregistro").addEventListener("click", function() {
         var nom = document.getElementById("nom").value;
         var username = document.getElementById("username_reg").value;
         var pwd = document.getElementById("pwd_reg").value;
@@ -97,25 +107,25 @@ document.addEventListener("DOMContentLoaded", function () {
             pwd: pwd
         };
         console.log(user2)
-        //enviar aquest objecte al servidor:
+            //enviar aquest objecte al servidor:
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", "./php/registro.php");//obrir connexio
-        xhr.send(JSON.stringify(user2));//enviament de dades: objeto a JSON antes del envio
-        xhr.onload = function () {//esperar a rebre dades
+        xhr.open("POST", "./php/registro.php"); //obrir connexio
+        xhr.send(JSON.stringify(user2)); //enviament de dades: objeto a JSON antes del envio
+        xhr.onload = function() { //esperar a rebre dades
 
             if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
                 alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
             } else { // muestra el resultado
                 //alert(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
                 //xhr.response es un JSON que viene desde PHP
-                let responseServer = JSON.parse(xhr.response);//reconvertirlo/parsearlo a variable JS
+                let responseServer = JSON.parse(xhr.response); //reconvertirlo/parsearlo a variable JS
                 document.getElementById("response").innerHTML = responseServer;
                 // if (responseServer == "Welcome Admin") { checkCookie("cookie", username); };
             }
             document.getElementById("divregistro").style.display = "none";
         }
     })
-    document.getElementById("btvuelos").addEventListener("click", function () {
+    document.getElementById("btvuelos").addEventListener("click", function() {
         document.getElementById("response").style.display = "block";
         // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         // var d = new Date(salida);
@@ -128,9 +138,13 @@ document.addEventListener("DOMContentLoaded", function () {
         var dayName = d.toString().split(' ')[0];
         console.log(dayName)
         var vuelta = document.getElementById("vuelta").value;
+        var di = new Date(vuelta);
+        var nameday = di.toString().split(' ')[0];
+        console.log(nameday)
         var pasajeros = document.getElementById("pasajeros").value;
         let viaje = {
             dayName: dayName,
+            nameday: nameday,
             origen: origen,
             destino: destino,
             salida: salida,
@@ -142,48 +156,77 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("rellena todos los campos")
         } else if (origen == destino) { alert("el orige y el destino son el mismo") } else { alert("buscando viajes") }
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", "./php/buscar.php");//obrir connexio
-        xhr.send(JSON.stringify(viaje));//enviament de dades: objeto a JSON antes del envio
-        xhr.onload = function () {//esperar a rebre dades
+        xhr.open("POST", "./php/buscar.php"); //obrir connexio
+        xhr.send(JSON.stringify(viaje)); //enviament de dades: objeto a JSON antes del envio
+        xhr.onload = function() { //esperar a rebre dades
 
-            if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
-                alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
-            } else { // muestra el resultado
-                //alert(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
-                //xhr.response es un JSON que viene desde PHP
-                let responseServer = JSON.parse(xhr.response);//reconvertirlo/parsearlo a variable JS
-                document.getElementById("info").innerHTML = responseServer;
-                // if (responseServer == "Welcome Admin") { checkCookie("cookie", username); };
+                if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
+                    alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+                } else { // muestra el resultado
+                    //alert(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
+                    //xhr.response es un JSON que viene desde PHP
+                    let responseServer = JSON.parse(xhr.response); //reconvertirlo/parsearlo a variable JS
+                    document.getElementById("info").innerHTML = responseServer;
+                    // if (responseServer == "Welcome Admin") { checkCookie("cookie", username); };
+                }
             }
-        }
-        //nuevo
-        // xhr.abort();
+            //nuevo
+            // xhr.abort();
         let ls = new XMLHttpRequest();
         ls.open("POST", "./php/salidas.php");
         ls.send(JSON.stringify(viaje));
-        ls.onload = function () {
+        ls.onload = function() {
 
-            if (ls.status != 200) {
-                alert(`Error ${ls.status}: ${ls.statusText}`);
+                if (ls.status != 200) {
+                    alert(`Error ${ls.status}: ${ls.statusText}`);
+                } else {
+                    let responseServer = JSON.parse(ls.response);
+                    document.getElementById("tt").innerHTML = responseServer; // cambiar tt por salidas
+                    // nuevo
+                    var data = JSON.parse(ls.response);
+
+                    for (var i = 0; i < data.length; i++) {
+                        var option = document.createElement("option");
+                        option.value = data[i];
+                        option.innerHTML = data[i];
+                        document.getElementById("salidas").appendChild(option)
+                    };
+
+
+                    ///
+                }
+            }
+            // ls.abort();
+            // lo de arriba hasta //nuevo es nuevo
+
+        //
+        let vu = new XMLHttpRequest();
+        vu.open("POST", "./php/vueltas.php");
+        vu.send(JSON.stringify(viaje));
+        vu.onload = function() {
+
+            if (vu.status != 200) {
+                alert(`Error ${vu.status}: ${vu.statusText}`);
             } else {
-                let responseServer = JSON.parse(ls.response);
+                let responseServer = JSON.parse(vu.response);
                 document.getElementById("tt").innerHTML = responseServer; // cambiar tt por salidas
                 // nuevo
-                var data = JSON.parse(ls.response);
+                var data = JSON.parse(vu.response);
 
                 for (var i = 0; i < data.length; i++) {
                     var option = document.createElement("option");
                     option.value = data[i];
                     option.innerHTML = data[i];
-                    document.getElementById("salidas").appendChild(option)
+                    document.getElementById("vueltas").appendChild(option)
                 };
 
 
                 ///
             }
         }
-        // ls.abort();
-        // lo de arriba hasta //nuevo es nuevo
+
+
+        ///
         if (origen == "" || destino == "" || salida == "" || vuelta == "" || pasajeros == "") {
             alert("rellena todos los campos")
         } else if (origen == destino) { alert("el orige y el destino son el mismo") } else {
@@ -198,6 +241,40 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     })
+
+    document.getElementById("btcomprar").addEventListener("click", function() {
+        var horasalida = document.getElementById("salidas").value;
+        document.getElementById("anada").innerHTML = horasalida;
+        var horavuelta = document.getElementById("vueltas").value;
+        document.getElementById("tornada").innerHTML = horavuelta;
+        let horarios = {
+            horasalida: horasalida,
+            horavuelta: horavuelta,
+        }
+        console.log(horarios);
+        document.getElementById("buscar").style.display = "none";
+        document.getElementById("usuario").style.display = "block";
+    })
+
+    document.getElementById("btcontinuar").addEventListener("click", function() {
+        var dni = document.getElementById("dni").value;
+        document.getElementById("dnie").innerHTML = dni;
+        var telefono = document.getElementById("telefono").value;
+        document.getElementById("mobil").innerHTML = telefono;
+        var correo = document.getElementById("correo").value;
+        document.getElementById("email").innerHTML = correo;
+        if (dni == "" || telefono == "" || correo == "") {
+            alert("rellena todos los campos");
+        } else {
+            document.getElementById("usuario").style.display = "none";
+            document.getElementById("datos").style.display = "block";
+        }
+    })
+
+    document.getElementById("print").addEventListener("click", function() {
+        window.print()
+    })
+
 });
 
 
@@ -296,7 +373,7 @@ function loadoptions() {
     let request = new XMLHttpRequest()
     request.open("GET", "./php/vuelos.php");
     request.send();
-    request.onload = function () {
+    request.onload = function() {
         var data = JSON.parse(request.response);
 
         for (var i = 0; i < data.length; i++) {
